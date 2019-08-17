80690d013844c1252ffb7145450920ffc2554543
p1st1s <emmanueladeboye2017@gmail.com>
2019-08-17 03:49:39 +0100

    start point for the project

---
+++ code/crawler.py
@@ -0,0 +1,5 @@
+"""
+This module contains all the code that will gather all the necessary
+details that is needed about a facebook user in other to make
+our predictions.
+"""
---
+++ code/model.py
@@ -0,0 +1,3 @@
+"""
+A model to predict the gender of a facebook user based on certain information
+"""
